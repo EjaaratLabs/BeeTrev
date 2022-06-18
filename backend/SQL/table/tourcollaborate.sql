c53bb454d7040a4ad270023da13b3e1587a3cc95
hunzalaali <hunzalaali8@gmail.com>
2022-06-18 15:00:56 +0500

    tables

--- backend/SQL/table/tourcollaborate.sql
+++ backend/SQL/table/tourcollaborate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2022 at 10:07 AM
+-- Generation Time: Jun 18, 2022 at 11:57 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -28,21 +28,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tourcollaborate` (
-  `id` int(10) NOT NULL,
+  `collaborationId` int(10) NOT NULL,
   `operatorId` varchar(255) NOT NULL,
   `tourId` int(10) NOT NULL,
-  `collaborateStatus` int(10) NOT NULL DEFAULT 0,
-  `price` int(100) NOT NULL,
-  `quantity` int(10) NOT NULL
+  `collaborateStatus` varchar(10) NOT NULL DEFAULT '0',
+  `collaborationPrice` int(100) NOT NULL,
+  `collaborationQuantity` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tourcollaborate`
 --
 
-INSERT INTO `tourcollaborate` (`id`, `operatorId`, `tourId`, `collaborateStatus`, `price`, `quantity`) VALUES
-(4, 'admin', 2, 0, 25000, 2),
-(5, 'ali123', 2, 0, 5555, 2);
+INSERT INTO `tourcollaborate` (`collaborationId`, `operatorId`, `tourId`, `collaborateStatus`, `collaborationPrice`, `collaborationQuantity`) VALUES
+(4, 'admin', 7, '1', 25000, 2),
+(5, 'ali123', 2, '2', 5555, 2),
+(6, 'hunzala', 3, '0', 33333, 4);
 
 --
 -- Indexes for dumped tables
@@ -52,7 +53,7 @@ INSERT INTO `tourcollaborate` (`id`, `operatorId`, `tourId`, `collaborateStatus`
 -- Indexes for table `tourcollaborate`
 --
 ALTER TABLE `tourcollaborate`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`collaborationId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -62,7 +63,7 @@ ALTER TABLE `tourcollaborate`
 -- AUTO_INCREMENT for table `tourcollaborate`
 --
 ALTER TABLE `tourcollaborate`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `collaborationId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
